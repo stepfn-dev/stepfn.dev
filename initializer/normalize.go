8f73e8af664b42c8c3f7511d80b6fb552ac8ea27
Aidan Steele <aidan.steele@glassechidna.com.au>
2021-03-11 13:40:33 +1100

    work with map states as well

---
+++ initializer/normalize.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"stepfndev/stepfndev"
+	"strings"
+)
+
+func normalizeStateMachineDefinition(definition string, dynamoId string, funcArn string) string {
+	transformed := definition
+	// TODO: validate state names
+	gjson.Get(definition, "States").ForEach(func(key, value gjson.Result) bool {
+		newValue := value.Raw
+		switch value.Get("Type").Str {
+		case "Task":
+			newValue = normalizeTaskState(value, dynamoId, funcArn)
+		case "Map":
+			iterator := normalizeStateMachineDefinition(value.Get("Iterator").Raw, dynamoId, funcArn)
+			newValue, _ = sjson.Set(value.Raw, "Iterator", json.RawMessage(iterator))
+		}
+
+		transformed, _ = sjson.Set(transformed, "States."+key.Str, json.RawMessage(newValue))
+		return true
+	})
+
+	return transformed
+}
+
+func normalizeTaskState(value gjson.Result, dynamoId, funcArn string) string {
+	if strings.HasPrefix(value.Get("Resource").Str, "arn:aws:states:::lambda:invoke") {
+		handler := gjson.Get(value.Raw, "Parameters.FunctionName").Str
+		cc := &stepfndev.ClientContext{Id: dynamoId, Handler: handler}
+		transformedValue, _ := sjson.Set(value.Raw, "Parameters.ClientContext", cc.Encode())
+		transformedValue, _ = sjson.Set(transformedValue, "Parameters.FunctionName", funcArn)
+		transformedValue, _ = sjson.Delete(transformedValue, "Parameters.FunctionName\\.$")
+		transformedValue, _ = sjson.Delete(transformedValue, "Parameters.Qualifier")
+		transformedValue, _ = sjson.Delete(transformedValue, "Parameters.Qualifier\\.$")
+
+		return transformedValue
+	} else {
+		// TODO: old style tasks with lambda arn resource
+		return ""
+	}
+}
---
+++ initializer/normalize_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"testing"
+)
+
+func TestNormalizeStateMachineDefinition_Example(t *testing.T) {
+	body, err := ioutil.ReadFile("testdata/example.json")
+	require.NoError(t, err)
+
+	expected, err := ioutil.ReadFile("testdata/example_expected.json")
+	require.NoError(t, err)
+
+	actual := normalizeStateMachineDefinition(string(body), "dynamoid", "funcarn")
+	assert.JSONEq(t, string(expected), actual)
+}
+
+func TestNormalizeStateMachineDefinition_MapState(t *testing.T) {
+	body, err := ioutil.ReadFile("testdata/mapstate.json")
+	require.NoError(t, err)
+
+	expected, err := ioutil.ReadFile("testdata/mapstate_expected.json")
+	require.NoError(t, err)
+
+	actual := normalizeStateMachineDefinition(string(body), "dynamoid", "funcarn")
+	assert.JSONEq(t, string(expected), actual)
+}
